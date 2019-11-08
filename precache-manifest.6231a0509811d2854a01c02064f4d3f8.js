af13e744cd68616dd26d5b84a9d9642d5c2afbbb
HeideQueen <lucyfabz@gmail.com>
2019-11-07 18:31:38 -0600

    Updates

---
+++ precache-manifest.6231a0509811d2854a01c02064f4d3f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61e187098d4cc3e09136ea707b47326",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "99715054b895e6bcff77",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cd06ac269cf54e73139d",
+    "url": "/robofriends/static/css/main.9949d620.chunk.css"
+  },
+  {
+    "revision": "99715054b895e6bcff77",
+    "url": "/robofriends/static/js/2.a69bf4fc.chunk.js"
+  },
+  {
+    "revision": "cd06ac269cf54e73139d",
+    "url": "/robofriends/static/js/main.f1d97b34.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
